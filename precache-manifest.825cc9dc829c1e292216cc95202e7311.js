eb79601fa516a755f6cc841e06ff149b67bed511
dsijakovski98 <dsijakovski98@github.com>
2020-10-07 02:14:46 +0200

    Updates

---
+++ precache-manifest.825cc9dc829c1e292216cc95202e7311.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438983931e33a85d493c890e43cfd12c",
+    "url": "/akva-dejon-plus/index.html"
+  },
+  {
+    "revision": "6e9178acc9dbf2a62028",
+    "url": "/akva-dejon-plus/static/css/main.8614a855.chunk.css"
+  },
+  {
+    "revision": "385298800b443d35533c",
+    "url": "/akva-dejon-plus/static/js/2.4355897e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/akva-dejon-plus/static/js/2.4355897e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e9178acc9dbf2a62028",
+    "url": "/akva-dejon-plus/static/js/main.1a91ff4d.chunk.js"
+  },
+  {
+    "revision": "71e02427164c2a11740b",
+    "url": "/akva-dejon-plus/static/js/runtime-main.93553d34.js"
+  },
+  {
+    "revision": "cfb9db0bf7406b0ebd4c003fc1992ada",
+    "url": "/akva-dejon-plus/static/media/akva-dejon-logo-transparent.cfb9db0b.png"
+  },
+  {
+    "revision": "a7b05a70f1203278b654a90744b0e3bc",
+    "url": "/akva-dejon-plus/static/media/body-bg.a7b05a70.jpg"
+  }
+]);
